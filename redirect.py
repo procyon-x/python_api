807822514748e6e19f51a3bc4beb3f60ba91ecfc
procyon-x <procyon.4x4@gmail.com>
2021-09-04 18:24:25 +0300

    Ex6: Длинный редирект

--- redirect.py
+++ redirect.py
@@ -1,10 +1,9 @@
 import requests
 
 response = requests.get("https://playground.learnqa.ru/api/long_redirect", allow_redirects=True)
-first_response = response.history
-second_response = response
+redirect_url = response.history
+final_url = response
 
-new_url = first_response
-for i in range(len(new_url)):
-    print(new_url[i].url)
-print(second_response.url)
+for i in range(len(redirect_url)):
+    print(redirect_url[i].url)
+print(final_url.url)
